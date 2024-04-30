1e21f21b7cd813947046df3d1cb1313c6199d8f7
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-04-30 09:39:17 -0400

    :tada: db remota y local

--- src/models/config/db.js
+++ src/models/config/db.js
@@ -1,32 +1,52 @@
-//<!-- db.js -->
+// <!-- db.js -->
 import pg from 'pg';
-const {Pool} = pg; 
+const { Pool } = pg;
 import chalk from 'chalk';
 import dotenv from 'dotenv';
-dotenv.config(); 
-// process.loadEnvFile(); // no es compatible en render para subir db de neon
- 
-//const { DB_HOST, DB_DATABASE, DB_PORT, DB_USER, DB_PASSWORD } = process.env; //db postgre
-const { PGHOST, PGDATABASE, DB_PORT, PGUSER, PGPASSWORD } = process.env; //db neon
+dotenv.config();
 
- const config = {
-     host: PGHOST,
-     database: PGDATABASE,
-     port: DB_PORT,   
-     user: PGUSER,
-     password: PGPASSWORD,     
-     allowExitOnIdle: true,  
-     ssl: {
-        rejectUnauthorized: false,
-      }
- }
- const pool = new Pool(config);
+// Cargamos las variables de entorno
+const { DB_HOST, DB_DATABASE, DB_PORT, DB_USER, DB_PASSWORD, PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
- pool.connect((err, client, release) => {
-     if (err) {
-         return console.error('Error al conectarse a la base de datos:', err.stack);
-     }
-     console.log(chalk.underline.bgCyanBright.magenta.bold.italic('\nConexión con la base de datos establecida.'));
-     release();
- });
-export default pool
+// Configuración para la base de datos local y remota
+const config = {
+    local: {
+        host: DB_HOST,
+        database: DB_DATABASE,
+        port: DB_PORT,
+        user: DB_USER,
+        password: DB_PASSWORD
+    },
+    remote: {
+        host: PGHOST,
+        database: PGDATABASE,
+        port: DB_PORT,
+        user: PGUSER,
+        password: PGPASSWORD,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+};
+
+// Función para obtener la configuración según el entorno (local o remoto)
+const getConfig = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return config.remote;
+    } else {
+        return config.local;
+    }
+};
+
+// Creamos el pool de conexiones utilizando la configuración correspondiente
+const pool = new Pool(getConfig());
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error al conectarse a la base de datos:', err.stack);
+    }
+    console.log(chalk.underline.bgCyanBright.magenta.bold.italic('\nConexión con la base de datos establecida.'));
+    release();
+});
+
+export default pool;
